321788c34b7f2f084191f21d05d0c64748d1d703
daferarte <daferarte@gmail.com>
2024-02-22 12:09:18 -0500

    Metodos

--- Empleado/Empleado.py
+++ Empleado/Empleado.py
@@ -11,3 +11,18 @@ class Empleado:
     sexo=0
     salario = 0
     
+    '''----------------------------------------------------------------
+    # Metodos
+    ----------------------------------------------------------------'''
+    
+    def CambiarSalario(self, nuevoSalario):
+        # Aqui va el codigo del metodo
+        return 0
+        
+    def CambiarEmpleado(self, nNombre, nApellido, nSexo, nSalario):
+        # Aqui va el codigo del nuevo empleado
+        return None
+    
+    def ConsultarSalario(self):
+        # Aqui va el codigo del metodo
+        return self.salario
